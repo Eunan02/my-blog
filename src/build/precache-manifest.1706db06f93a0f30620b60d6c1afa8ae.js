9379af10a328dc6901aa1da321070f99af26bfa8
Eunan Diamond <eunan.diamond@oracle.com>
2022-08-19 13:12:05 +0100

    first commit

---
+++ src/build/precache-manifest.1706db06f93a0f30620b60d6c1afa8ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02d464e87aad594ea3ed",
+    "url": "/static/css/main.fac8d931.chunk.css"
+  },
+  {
+    "revision": "02d464e87aad594ea3ed",
+    "url": "/static/js/main.3529c837.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dffda320f2d66c675dda",
+    "url": "/static/js/2.4ce7a0ca.chunk.js"
+  },
+  {
+    "revision": "9805a0908f7038539a6ebb30197bb5af",
+    "url": "/index.html"
+  }
+];
